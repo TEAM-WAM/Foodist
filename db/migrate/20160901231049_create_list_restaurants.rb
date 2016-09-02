7149d44686288a2dd9726b21638e6ddc3ddc7e73
Myra Orgain & Walter Chabla <pair+mlorgain.walterchabla@devbootcamp.com>
2016-09-02 17:44:05 -0400

    Add validations and clean up tables

--- db/migrate/20160901231049_create_list_restaurants.rb
+++ db/migrate/20160901231049_create_list_restaurants.rb
@@ -1,6 +1,7 @@
 class CreateListRestaurants < ActiveRecord::Migration[5.0]
   def change
     create_table :list_restaurants do |t|
+      t.string :name, null: false, limit: 100
       t.string :cuisine, limit: 100
       t.string :fav_dish, limit: 100
       t.string :restaurant_type, limit: 100
