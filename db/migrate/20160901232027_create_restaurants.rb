6c8c2d226477cc5176347fb1aba97092affa362d
Myra Orgain & Walter Chabla <pair+mlorgain.walterchabla@devbootcamp.com>
2016-09-02 17:44:05 -0400

    Add validations and clean up tables

--- db/migrate/20160901232027_create_restaurants.rb
+++ db/migrate/20160901232027_create_restaurants.rb
@@ -1,13 +1,13 @@
 class CreateRestaurants < ActiveRecord::Migration[5.0]
   def change
     create_table :restaurants do |t|
-      t.string :name, null: false
+      t.string :name, null: false, limit: 100
       t.string :restaurant_url
-      t.string :address
-      t.string :locality
-      t.string :city
-      t.string :zipcode
-      t.string :average_cost_for_two
+      t.string :address, null: false, limit: 200
+      t.string :locality, limit: 100
+      t.string :city, null: false, limit: 100
+      t.string :zipcode, limit: 10
+      t.string :average_cost_for_two, limit: 10
       t.string :featured_img
       t.string :menu_url
       t.boolean :has_online_delivery
