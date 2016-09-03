2e64536620dda5dfac87bd0229c0ff8fa4715e5f
Myra Orgain & Walter Chabla <pair+mlorgain.walterchabla@devbootcamp.com>
2016-09-03 19:27:17 -0400

    We did stuff, such as fixing all the validation that we messed up

--- app/models/list_experience.rb
+++ app/models/list_experience.rb
@@ -4,10 +4,8 @@ class ListExperience < ApplicationRecord
   has_many :custom_values, as: :customable_value
 
   validates :list_restaurant_id, :date_of_experience, presence: true
-  validates :date_of_experience, on_or_before: lambda { Date.current }
-  validates :date_of_experience, length { maximum: 50 }
-  validates :main_dish, length { maximum: 100 }
-  validates :notes, length { maximum: 800 }
-  validates_date :date_of_experience
+  validates :date_of_experience, length: { maximum: 50 }
+  validates :main_dish, length: { maximum: 100 }
+  validates :notes, length: { maximum: 800 }
 
 end
