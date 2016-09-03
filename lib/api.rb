873ddf9692ef714fc5f1895581c8e552e4b85a17
Myra Orgain & Walter Chabla <pair+mlorgain.walterchabla@devbootcamp.com>
2016-09-03 13:51:39 -0400

    Finally got the zomato api module to work :D

---
+++ lib/api.rb
@@ -0,0 +1,24 @@
+module Zomato
+
+    class API
+
+        include HTTParty
+        format :json
+        base_uri 'https://developers.zomato.com/api/v2.1'
+        default_params :format => 'json'
+
+        def initialize(entity_id, entity_type, collection_id)
+            @headers = { "user-key" => "f5e327252d7eaeb8a135c2804646707d" }
+            @options = {"entity_id" => entity_id, "entity_type" => entity_type, "collection_id" => collection_id}
+        end
+
+        def request_data
+        	@response = self.class.get("/search", :query => @options, :headers => @headers)
+        	json = JSON.parse(@response.body)
+        	puts json['response.body']
+        end
+
+    end
+
+end
+
