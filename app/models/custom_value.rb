9d54ea9e9ba6ffcfea85b4900799bc6da42d7d23
Myra Orgain & Walter Chabla <pair+mlorgain.walterchabla@devbootcamp.com>
2016-09-03 19:27:17 -0400

    We did stuff, such as fixing all the validation that we messed up

--- app/models/custom_value.rb
+++ app/models/custom_value.rb
@@ -4,6 +4,6 @@ class CustomValue < ApplicationRecord
   belongs_to :customable_value, polymorphic: true
 
   validates :field_value, :custom_field_id, :customable_value_type, :customable_value_id, presence: true
-  validates :field_value, length { maximum: 100 }
+  validates :field_value, length: { maximum: 100 }
 
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,10 +3,9 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true
-  validates :zipcode, length { maximum: 10 }
-  validates :phone_number, length { maximum: 16 }
-  validates :gender, length { maximum: 50 }
-  validates :bio, length { maximum: 300 }
-  validates_date :birthday
+  validates :zipcode, length: { maximum: 10 }
+  validates :phone_number, length: { maximum: 16 }
+  validates :gender, length: { maximum: 50 }
+  validates :bio, length: { maximum: 300 }
 
 end
