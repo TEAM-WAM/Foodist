86847113e824fc21b23a352304f6bd25ea10f1b5
Walter Chabla <walterchabla43@gmail.com>
2016-09-03 16:23:47 -0400

    fixed confirmation password validation to be present

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:username, :email, :password, :first_name, :last_name)
+      user_params.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name)
     end
   end
 end
