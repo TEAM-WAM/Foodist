019b5bd16e0b710a844baad720d314c368e40006
Walter Chabla <walterchabla43@gmail.com>
2016-09-04 16:48:02 -0400

    able to add members and upload a picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902193732) do
+ActiveRecord::Schema.define(version: 20160904190742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +42,15 @@ ActiveRecord::Schema.define(version: 20160902193732) do
   end
 
   create_table "groups", force: :cascade do |t|
-    t.string   "name",        limit: 50, null: false
-    t.text     "description",            null: false
-    t.integer  "creator_id",             null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",                limit: 50, null: false
+    t.text     "description",                    null: false
+    t.integer  "creator_id",                     null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "groups_users", id: false, force: :cascade do |t|
