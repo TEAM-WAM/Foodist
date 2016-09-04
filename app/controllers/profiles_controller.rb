5fc578b70a14fd0332523cf96b8710835faa1b58
Myra Orgain <email": null>
2016-09-04 17:26:22 -0400

    Added paperclip, a bootstrap theme, and fleshed out user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,23 @@
 class ProfilesController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    if @user.profile
+      @profile = Profile.find_by(user_id: params[:id])
+    else
+      new_profile = @user.build_profile
+      new_profile.save
+      @profile = Profile.find_by(user_id: params[:id])
+    end
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
 end
