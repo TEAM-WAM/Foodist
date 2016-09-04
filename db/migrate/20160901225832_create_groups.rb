be1f843decc4e3d5480fd54934c8c1beae943bdb
Myra Orgain <email": null>
2016-09-04 18:42:49 -0400

    Fix group migration

--- db/migrate/20160901225832_create_groups.rb
+++ db/migrate/20160901225832_create_groups.rb
@@ -4,7 +4,6 @@ class CreateGroups < ActiveRecord::Migration[5.0]
       t.string :name, null: false, limit: 50
       t.text :description, null: false, limit: 300
       t.integer :creator_id, null: false
-      t.att
 
       t.timestamps null: false
     end
