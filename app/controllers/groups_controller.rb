53f71745931f191060a050f588c7163de669fab8
Walter Chabla <walterchabla43@gmail.com>
2016-09-04 16:48:02 -0400

    able to add members and upload a picture

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @groups = Group.all
   end
@@ -7,4 +9,28 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.new(group_params)
+    group.save
+      redirect_to groups_path
+  end
+
+  def add
+    @user = User.find(params[:user_id])
+    @group= Group.find(params[:group_id])
+    @group.members << @user
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :description, :creator_id, :avatar)
+  end
+
 end
--- db/migrate/20160901225832_create_groups.rb
+++ db/migrate/20160901225832_create_groups.rb
@@ -4,7 +4,7 @@ class CreateGroups < ActiveRecord::Migration[5.0]
       t.string :name, null: false, limit: 50
       t.text :description, null: false, limit: 300
       t.integer :creator_id, null: false
-      t.string :image, default: 'assets/images/group_food_profile.jpg'
+      t.att
 
       t.timestamps null: false
     end
