25da18380e3d22bc304ef1aaa7b8bad1a628fafb
Myra Orgain <email": null>
2016-09-05 18:57:54 -0400

    Fleshed out restaurants index page with most voted on places

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:show, :index]
+
+  def index
+    @restaurants = Restaurant.all
+    all_rests = []
+    @restaurants.each do |rest|
+      all_rests << { rest.name => rest.votes.count }
+    end
+    all_rests.sort_by { |name, votes| votes }
+    top_ten = all_rests[0...10]
+    @top_ten_obj = []
+    top_ten.map do |rest|
+      @top_ten_obj << Restaurant.find_by(name: rest.keys )
+    end
+  end
+
+  def show
+
+  end
+
 end
