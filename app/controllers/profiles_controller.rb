86953abdb6e18e588d9b4a35c88899b7e60d126f
Walter Chabla <walterchabla43@gmail.com>
2016-09-04 22:19:18 -0400

    add edit functionally and a devise method to profile controllers

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,25 @@
 class ProfilesController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-    if @user.profile
-      @profile = Profile.find_by(user_id: params[:id])
-    else
-      new_profile = @user.build_profile
-      new_profile.save
-      @profile = Profile.find_by(user_id: params[:id])
-    end
+  before_action :authenticate_user!, only: [:show, :edit, :update]
 
+  def show
+    # @user = User.find(params[:id])
+    # if @user.profile
+    #   @profile = Profile.find_by(user_id: params[:id])
+    # else
+    #   new_profile = @user.build_profile
+    #   new_profile.save
+    #   @profile = Profile.find_by(user_id: params[:id])
+    # end
+    @profile = Profile.find_by(user_id: params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
 end
