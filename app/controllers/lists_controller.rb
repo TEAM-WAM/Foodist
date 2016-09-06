e1f308fdbc3d5291eb61d39b03f77f235b7517a5
A-Tawfik <taamer@gmail.com>
2016-09-06 10:24:30 -0400

    commit for rebase

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,10 +7,23 @@ class ListsController < ApplicationController
   def index
   end
 
-  def trending
-    # @lists = List.where(:votes ) limit 10
-    # render 'trending.jbuilder.json'
-    # @user = User.find(current_user.id)
+  def jgroup
+    group = Group.find_by(id: params[:id])
+    @lists = group.lists
+    render 'index.json.jbuilder'
+  end
+
+  def jtrending
+    lists_array = []
+    @lists = []
+    all_lists = List.all
+    all_lists.each do |list|
+      votes = list.votes.where(up: 'true').count - list.votes.where(up: 'false').count
+      lists_array << {list => votes}
+    end
+    trending = lists_array.sort{ |a,b| a.values.flatten <=> b.values.flatten}.reverse[0..10]
+    trending.each {|list| @lists << list.keys[0]}
+    render 'index.json.jbuilder'
   end
 
   def jshow
