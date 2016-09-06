e30a5621a02b7cab1ed9ba7510f729563a876e5d
Myra Orgain <email": null>
2016-09-06 16:52:09 -0400

    Made a pencil button

--- app/assets/javascripts/components/Comment.es6.jsx
+++ app/assets/javascripts/components/Comment.es6.jsx
@@ -2,7 +2,7 @@ class Comment extends React.Component{
   render(){
     var link = '/profiles/' + this.props.data.user.profile_id
     return(
-          <li>
+          <li className="list-group-item">
           <div>
             <a href= {link}>{this.props.data.user.username}</a>
           </div>
