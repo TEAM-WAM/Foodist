d7f558725f3272de388a07478d76df2defbea0b6
Walter Chabla <walterchabla43@gmail.com>
2016-09-07 19:59:45 -0400

    finish search bar drag and drop

--- app/controllers/list_restaurants_controller.rb
+++ app/controllers/list_restaurants_controller.rb
@@ -3,7 +3,12 @@ class ListRestaurantsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @list.restaurants << @restaurant
+    list_restaurant = @list.restaurants.where(restaurant_id: @restaurant.id)
+
+    if list_restaurant.length == 0
+      @list.restaurants << @restaurant
+    end
+
   end
 
 end
