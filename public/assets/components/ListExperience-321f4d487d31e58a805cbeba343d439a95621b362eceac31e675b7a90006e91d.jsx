d7741b3891c125429a11dadd6ee82558ef567f41
Walter Chabla <walterchabla43@gmail.com>
2016-09-07 14:29:29 -0400

    command to make heroku work with css

---
+++ public/assets/components/ListExperience-321f4d487d31e58a805cbeba343d439a95621b362eceac31e675b7a90006e91d.jsx
@@ -0,0 +1,10 @@
+class ListExperience extends React.Component{
+  render(){
+    return(
+      <tr className="ListExperience">
+        <td>{this.props.data.date_of_experience}</td>
+      </tr>
+    )
+  }
+
+}
