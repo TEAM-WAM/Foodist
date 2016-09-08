37d2b94b5c640aa2ca7215a96dc463878829aa75
Walter Chabla <walterchabla43@gmail.com>
2016-09-07 20:05:41 -0400

    profile avatar content type changed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -9,6 +9,8 @@ class Profile < ApplicationRecord
   validates :phone_number, length: { maximum: 16 }
   validates :gender, length: { maximum: 50 }
   validates :bio, length: { maximum: 300 }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 
 end
