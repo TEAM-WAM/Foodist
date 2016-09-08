483e96bc0fc9d825cf8cecaa7385d04e4fe61a54
Myra Orgain <email": null>
2016-09-07 21:29:31 -0400

    add experiences form

--- app/controllers/list_experiences_controller.rb
+++ app/controllers/list_experiences_controller.rb
@@ -1,2 +1,13 @@
 class ListExperiencesController < ApplicationController
+
+  def create
+    experience = ListExperience.create(list_experience_params)
+    render json: experience
+  end
+
+  private
+  def list_experience_params
+    params.require(:data).permit(:main_dish, :price, :party_size, :time_waiting, :notes, :date_of_experience, :list_restaurant_id)
+  end
+
 end
