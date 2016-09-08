0c8632e58d1b75b04e9c682a4e9c334d84e3e475
Walter Chabla <walterchabla43@gmail.com>
2016-09-07 20:20:13 -0400

    updated search html file

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -9,8 +9,6 @@ class Profile < ApplicationRecord
   validates :phone_number, length: { maximum: 16 }
   validates :gender, length: { maximum: 50 }
   validates :bio, length: { maximum: 300 }
-  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 end
